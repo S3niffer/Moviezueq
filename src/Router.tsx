22d6db4c7e7e0b84f6d96376782bde5158cc39e1
Erphun <heydar858@gmail.com>
2024-06-10 21:02:32 +0330

    changed: names

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
 import { RouteObject, useRoutes } from "react-router-dom"
-import Entrance from "./Pages/Entrance"
+import Entrance from "./Pages/Entrance/Entrance"
+import Home from "./Pages/Home/Home"
 
 const Routes: RouteObject[] = [
-    { path: "/sign-up", element: <Entrance /> },
-    { path: "/sign-in", element: <Entrance /> },
-    { path: "/", element: <div>HomePage</div> },
+    { path: "/entrance", element: <Entrance /> },
+    { path: "/", element: <Home /> },
 ]
 
 export default () => useRoutes(Routes)
