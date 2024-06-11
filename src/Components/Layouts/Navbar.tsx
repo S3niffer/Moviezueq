e99cc60cf369a0814c67c99f188f3e86b9c5f68e
Erphun <heydar858@gmail.com>
2024-06-11 19:49:10 +0330

    changed: classes and classNames

--- src/Components/Layouts/Navbar.tsx
+++ src/Components/Layouts/Navbar.tsx
@@ -3,7 +3,7 @@ import LanguageSelector from "./LanguageSelector"
 
 const Navbar = () => {
     return (
-        <div className='bg-added-oxfordblue h-navBarHeight'>
+        <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-50 shadow shadow-added-charcoal'>
             <nav className='container py-1 flex items-center justify-between 480:py-1.5 sm:py-2.5 h-full '>
                 <div className='w-1/3'>خوش آمدید</div>
                 <Logo className='w-1/4 text-center' />
--- src/Pages/Entrance/Entrance.tsx
+++ src/Pages/Entrance/Entrance.tsx
@@ -11,30 +11,32 @@ const Entrance = () => {
         setFormName(prv => (prv === "login" ? "register" : "login"))
     }
     return (
-        <div className='bg-added-delftblue h-screen'>
-            <Navbar />
-            <div className='h-[calc(100vh-var(--navbar-height))] relative'>
-                <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 entranceCardContainer'>
-                    <div
-                        className={`entranceCard relative transition-transform duration-500 ${
-                            formName === "register" ? " rotateCard " : ""
-                        }`}
-                    >
-                        {formName !== "register" ? (
-                            <EntranceCardContent
-                                cardName='عضویت'
-                                cardClass='entranceCard-register'
-                                Form={RegisterForm}
-                                formChanger={_formChanger}
-                            />
-                        ) : (
-                            <EntranceCardContent
-                                cardName='ورود'
-                                cardClass='entranceCard-login'
-                                Form={LoginForm}
-                                formChanger={_formChanger}
-                            />
-                        )}
+        <div className='h-screen overflow-y-auto ltr'>
+            <div className='rtl'>
+                <Navbar />
+                <div className='relative h-[380px] 480:h-[415px] sm:h-[480px] md:h-[540px] lg:h-[610px] mb-8 480:mb-16 sm:mb-24 md:mb-32 lg:mb-48'>
+                    <div className='absolute left-1/2 top-[calc(var(--navbar-height)+32px)] 480:top-[calc(var(--navbar-height)+48px)] sm:top-[calc(var(--navbar-height)+64px)] md:top-[calc(var(--navbar-height)+80px)] lg:top-[calc(var(--navbar-height)+96px)] -translate-x-1/2  z-10 entranceCardContainer'>
+                        <div
+                            className={`entranceCard relative transition-transform duration-500 ${
+                                formName === "register" ? " rotateCard " : ""
+                            }`}
+                        >
+                            {formName !== "register" ? (
+                                <EntranceCardContent
+                                    cardName='عضویت'
+                                    cardClass='entranceCard-register'
+                                    Form={RegisterForm}
+                                    formChanger={_formChanger}
+                                />
+                            ) : (
+                                <EntranceCardContent
+                                    cardName='ورود'
+                                    cardClass='entranceCard-login'
+                                    Form={LoginForm}
+                                    formChanger={_formChanger}
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
