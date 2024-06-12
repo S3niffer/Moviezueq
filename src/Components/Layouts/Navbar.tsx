deafca7e7555a6747d30f66dcc0e54cad656603b
Erphun <heydar858@gmail.com>
2024-06-12 19:13:32 +0330

    feat: changed navbar items if user logged-in

--- src/Components/Layouts/Navbar.tsx
+++ src/Components/Layouts/Navbar.tsx
@@ -1,15 +1,32 @@
 import Logo from "../Logo"
 import LanguageSelector from "./LanguageSelector"
+import SearchMovieInput from "./SearchMovieInput"
 
 const Navbar = () => {
-    return (
-        <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-50 shadow shadow-added-charcoal'>
-            <nav className='container py-1 flex items-center justify-between 480:py-1.5 sm:py-2.5 h-full '>
-                <div className='w-1/3'>خوش آمدید</div>
-                <Logo className='w-1/4 text-center' />
-                <LanguageSelector className='w-1/3 text-end' />
-            </nav>
-        </div>
-    )
+    const isLogin = true
+
+    if (isLogin) {
+        return (
+            <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-50 shadow shadow-added-charcoal'>
+                <nav className='container py-1 flex items-center justify-between 480:py-1.5 sm:py-2.5 h-full '>
+                    <Logo className='w-1/5' />
+                    <div className='w-3/6 pr-1'>
+                        <SearchMovieInput />
+                    </div>
+                    <LanguageSelector className='w-1/4 480:w-1/5 text-end mb-1 480:mb-1.5 sm:mb-2' />
+                </nav>
+            </div>
+        )
+    } else {
+        return (
+            <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-50 shadow shadow-added-charcoal'>
+                <nav className='container py-1 flex items-center justify-between 480:py-1.5 sm:py-2.5 h-full '>
+                    <div className='w-1/3'>خوش آمدید</div>
+                    <Logo className='w-1/4 text-center' />
+                    <LanguageSelector className='w-1/3 text-end mb-1 480:mb-1.5 sm:mb-2' />
+                </nav>
+            </div>
+        )
+    }
 }
 export default Navbar
