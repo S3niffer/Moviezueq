07e62db7194e170fddefc94de022d456ac0acee5
Erphun <heydar858@gmail.com>
2024-06-16 22:28:13 +0330

    changed: refactored style of entrance page

--- src/Pages/Entrance/Layouts/EntranceCard.tsx
+++ src/Pages/Entrance/Layouts/EntranceCard.tsx
@@ -2,7 +2,7 @@ const EntranceCard = ({ cardName, cardClass, Form, formChanger }: EntranceCard) 
     return (
         <div className={`bg-added-oxfordblue p-1.5 480:p-3 sm:p-6 md:p-8 rounded ${cardClass}`}>
             <p className='text-center'>{cardName}</p>
-            <div className='mt-2'>
+            <div className='mt-2 flex justify-center'>
                 <Form buttonValue={cardName} />
             </div>
             <span>
