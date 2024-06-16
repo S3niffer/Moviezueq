09dad3ae6bc4e5f0a78ca685761265781027851e
Erphun <heydar858@gmail.com>
2024-06-16 19:55:39 +0330

    feat: made private route and make home page private

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,18 @@
 import { RouteObject, useRoutes } from "react-router-dom"
 import Entrance from "./Pages/Entrance/Entrance"
 import Home from "./Pages/Home/Home"
+import PrivateRoute from "./Components/PrivateRoute"
 
 const Routes: RouteObject[] = [
     { path: "/entrance", element: <Entrance /> },
-    { path: "/", element: <Home /> },
+    {
+        path: "/",
+        element: (
+            <PrivateRoute>
+                <Home />
+            </PrivateRoute>
+        ),
+    },
 ]
 
 export default () => useRoutes(Routes)
