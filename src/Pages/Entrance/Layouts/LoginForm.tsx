a4650d12e596a792643490f7eb1185f68fedd8b3
Erphun <heydar858@gmail.com>
2024-06-17 16:31:57 +0330

    feat: add toggle password input visibility to loging and register form with  usePasswordVisibilityToggle hook

--- src/Pages/Entrance/Layouts/LoginForm.tsx
+++ src/Pages/Entrance/Layouts/LoginForm.tsx
@@ -5,6 +5,7 @@ import YInputButton from "../../../Components/YInputButton"
 import FieldErrorMessage from "./FieldErrorMessage"
 import { useMutation } from "@tanstack/react-query"
 import useloginByToken from "../../../_utils/useloginByToken"
+import usePasswordVisibilityToggle from "../../../_utils/usePasswordVisibilityToggle"
 
 const LoginForm = ({ buttonValue }: EntranceFormProps) => {
     const Form = useForm<LoginFormInputs>({ resolver: yupResolver(Login_Schema) })
@@ -33,6 +34,8 @@ const LoginForm = ({ buttonValue }: EntranceFormProps) => {
         _GetToken(FormData)
     }
 
+    const { icon, inputType } = usePasswordVisibilityToggle()
+
     return (
         <form
             className='flex items-center flex-col gap-1 480:gap-1.5 sm:gap-2 md:gap-2.5 lg:gap-3 mb-2 px-2 w-48 480:w-64 sm:w-80 lg:w-96'
@@ -42,18 +45,19 @@ const LoginForm = ({ buttonValue }: EntranceFormProps) => {
                 <input
                     type='text'
                     placeholder='پست الکترونیکی'
-                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] w-11/12 placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
+                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
                     {...Form.register("email")}
                 />
                 <FieldErrorMessage Field={Form.formState.errors.email} />
             </div>
-            <div className='w-full flex flex-col gap-1'>
+            <div className='w-full flex flex-col gap-1 relative'>
                 <input
-                    type='password'
+                    type={inputType}
                     placeholder='گذرواژه'
-                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] w-11/12'
+                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] pl-5 480:pl-6 sm:pl-7 md:pl-8 lg:pl-9 placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
                     {...Form.register("password")}
                 />
+                {icon}
                 <FieldErrorMessage Field={Form.formState.errors.password} />
             </div>
             <YInputButton
--- src/Pages/Entrance/Layouts/RegisterForm.tsx
+++ src/Pages/Entrance/Layouts/RegisterForm.tsx
@@ -5,6 +5,7 @@ import { Register_Schema } from "./yup_schemas"
 import FieldErrorMessage from "./FieldErrorMessage"
 import { useMutation } from "@tanstack/react-query"
 import { useNavigate } from "react-router-dom"
+import usePasswordVisibilityToggle from "../../../_utils/usePasswordVisibilityToggle"
 
 const RegisterForm = ({ buttonValue }: EntranceFormProps) => {
     const Form = useForm<RegisterFormInputs>({ resolver: yupResolver(Register_Schema) })
@@ -35,6 +36,8 @@ const RegisterForm = ({ buttonValue }: EntranceFormProps) => {
         _RegisterHandler(FormData)
     }
 
+    const { icon, inputType } = usePasswordVisibilityToggle()
+
     return (
         <form
             className='flex items-center flex-col gap-1 480:gap-1.5 sm:gap-2 md:gap-2.5 lg:gap-3 mb-2 px-2 w-48 480:w-64 sm:w-80 lg:w-96'
@@ -44,25 +47,26 @@ const RegisterForm = ({ buttonValue }: EntranceFormProps) => {
                 <input
                     type='text'
                     placeholder='نام'
-                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] w-11/12'
+                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px]'
                     {...Form.register("name")}
                 />
                 <FieldErrorMessage Field={Form.formState.errors.name} />
             </div>
-            <div className='w-full flex flex-col gap-1'>
+            <div className='w-full flex flex-col gap-1 relative'>
                 <input
-                    type='password'
+                    type={inputType}
                     placeholder='گذرواژه'
-                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] w-11/12'
+                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] pl-5 480:pl-6 sm:pl-7 md:pl-8 lg:pl-9 placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
                     {...Form.register("password")}
                 />
+                {icon}
                 <FieldErrorMessage Field={Form.formState.errors.password} />
             </div>
             <div className='w-full flex flex-col gap-1'>
                 <input
                     type='text'
                     placeholder='پست الکترونیکی'
-                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] w-11/12 placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
+                    className='outline-none px-1.5 bg-transparent border-b border-b-added-slategray text-added-slategray focus:border-b-added-schoolbus focus:text-added-white transition-all duration-300 480:border-b-2 md:border-b-[3px] placeholder:text-base placeholder:480:text-xl placeholder:sm:text-2xl placeholder:md:text-3xl placeholder:lg:text-4xl placeholder:font-FaFont font-EnFont h-6 480:h-7 sm:h-8 md:h-9 lg:h-10 text-xs 480:text-sm sm:text-base md:text-lg lg:text-2xl'
                     {...Form.register("email")}
                 />
                 <FieldErrorMessage Field={Form.formState.errors.email} />
