98170978becd2253be3e2aa83181329517a2472b
Erphun <heydar858@gmail.com>
2024-06-19 02:09:15 +0330

    changed: breadcrum icon image instead of text and added gap

--- src/Pages/SingleMovie/Layouts/BreadCrumb.tsx
+++ src/Pages/SingleMovie/Layouts/BreadCrumb.tsx
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom"
 import MoviezueqBox from "../../../Components/MoviezueqBox"
+import breadCrumbIcon from '/Pics/Breadcrumb.png'
 
 const BreadCrumb = ({ movieId }: { movieId: string | undefined }) => {
     return (
         <div className='container'>
             <MoviezueqBox>
-                <Link
-                    to={"/"}
-                    className='hover:text-added-schoolbus transition-colors'
-                >
-                    همه فیلم ها
-                </Link>
-                {" > "}
-                <Link
-                    to={`/movie/${movieId}`}
-                    className='text-added-schoolbus'
-                    aria-disabled
-                >
-                    فیلم شماره {movieId}
-                </Link>
+                <div className="flex items-center">
+                    <Link
+                        to={"/"}
+                        className='hover:text-added-schoolbus transition-colors'
+                    >
+                        همه فیلم ها
+                    </Link>
+                    <div className='w-10 aspect-square sm:w-12 md:w-14'>
+                        <img src={breadCrumbIcon} alt="flash icon" />
+                    </div>
+                    <Link
+                        to={`/movie/${movieId}`}
+                        className='text-added-schoolbus'
+                        aria-disabled
+                    >
+                        فیلم شماره {movieId}
+                    </Link>
+                </div>
             </MoviezueqBox>
         </div>
     )
