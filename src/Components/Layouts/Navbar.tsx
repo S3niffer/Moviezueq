d72d5d2cbc2fe90e4f6fb43bd13bc5c9abc083f5
Erphun <heydar858@gmail.com>
2024-06-19 02:41:31 +0330

    changed: navbar z index from 50 to 30

--- src/Components/Layouts/Navbar.tsx
+++ src/Components/Layouts/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     if (isLoggedIn) {
         return (
-            <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-50 shadow shadow-added-charcoal'>
+            <div className='bg-added-oxfordblue h-navBarHeight fixed left-0 right-0 top-0 z-30 shadow shadow-added-charcoal'>
                 <nav className='container py-1 flex items-center justify-between 480:py-1.5 sm:py-2.5 h-full '>
                     <Logo className='w-1/5' />
                     <div className='w-3/6 pr-1'>
