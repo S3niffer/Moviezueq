4373898420e96155cc934b2af05d9536aff27317
Erphun <heydar858@gmail.com>
2024-06-19 19:05:11 +0330

    feat: now _formatDuration return duration  with more detail (hour and minute ) if its more than 60m

--- src/_utils/_formatDuration.ts
+++ src/_utils/_formatDuration.ts
@@ -1,7 +1,13 @@
 const _formatDuration = (value: string) => {
     const withoutAffix = value.split(" ")[0]
 
-    return withoutAffix + "m"
+    if (+withoutAffix < 60) {
+        return withoutAffix + "m"
+    } else {
+        if (+withoutAffix == 60) return Math.trunc(+withoutAffix / 60) + "h"
+
+        return Math.trunc(+withoutAffix / 60) + "h " + (+withoutAffix % 60).toFixed(0) + "m"
+    }
 }
 
 export default _formatDuration
