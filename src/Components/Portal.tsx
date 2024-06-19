4d10dd80de94a385d23e70c6fc3e75657dd3c90a
Erphun <heydar858@gmail.com>
2024-06-19 13:25:16 +0330

    removed parent tag in portal let children decide what should be render

--- src/Components/Portal.tsx
+++ src/Components/Portal.tsx
@@ -3,10 +3,6 @@ import { createPortal } from "react-dom"
 
 const container = document.getElementById("root2")!
 
-const Parent = ({ children }: { children: ReactNode }) => (
-    <div className='fixed z-40 top-0 left-0 bottom-0 right-0'>{children}</div>
-)
-
-const Portal = ({ children }: { children: ReactNode }) => createPortal(<Parent>{children}</Parent>, container)
+const Portal = ({ children }: { children: ReactNode }) => createPortal(children, container)
 
 export default Portal
