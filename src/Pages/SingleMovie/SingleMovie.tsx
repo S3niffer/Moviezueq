4a452ccd26a39f6d249339f852a379e1d7065e05
Erphun <heydar858@gmail.com>
2024-06-20 15:53:48 +0330

    prevent: a bug in show singlemovie detail for those movie with id more than 250(the users added)

--- src/Pages/SingleMovie/SingleMovie.tsx
+++ src/Pages/SingleMovie/SingleMovie.tsx
@@ -8,6 +8,7 @@ import BreadCrumb from "./Layouts/BreadCrumb"
 import DetailsSection from "./Layouts/DetailsSection"
 import ImdbSection from "./Layouts/ImdbSection"
 import Poster from "./Layouts/Poster"
+import noImageFound from "/Pics/noImageFound.jpg"
 
 const SingleMovie = () => {
     const { movieId } = useParams()
@@ -16,6 +17,55 @@ const SingleMovie = () => {
         queryKey: ["Movie", movieId],
         queryFn: () => fetch(`https://moviesapi.ir/api/v1/movies/${movieId}`).then(res => res.json()),
     })
+
+    if (movieId && +movieId > 250) {
+        return (
+            <div className='rtl pt-[calc(var(--navbar-height)*1.5)]'>
+                <Navbar />
+                <BreadCrumb movieId={movieId} />
+                <div className='container pb-[var(--yellow-BG)] mt-[var(--navbar-height)]'>
+                    <MoviezueqBox>
+                        <div className='text-center my-3'>
+                            ! فیلم هایی که با ID بزرگتر از 250 هستن رو به این صورت نمایش داده میشود چون توسط توسعه دهنده اصلی Api
+                            قرار داده نشدند و همه پراپرتی های مورد نیاز رو ندارند.
+                            <hr />
+                        </div>
+                        {isLoading ? (
+                            <Loading />
+                        ) : data ? (
+                            <div className='flex gap-3 items-stretch'>
+                                <div className='flex justify-center'>
+                                    <div className='w-32 480:w-36 sm:w-44 aspect-[2/3] outline overflow-hidden outline-added-slategray schoolbus rounded md:w-52 lg:w-72'>
+                                        <img
+                                            src={noImageFound}
+                                            alt={`${data.title} movie poster`}
+                                            className='w-full h-full'
+                                        />
+                                    </div>
+                                </div>
+                                <div className='flex-1 flex flex-col justify-between'>
+                                    <div className='flex items-center justify-between'>
+                                        <p>عنوان</p>
+                                        <span>{data.title}</span>
+                                    </div>
+                                    <div className='flex items-center justify-between'>
+                                        <p>سال</p>
+                                        <span>{data.year}</span>
+                                    </div>
+                                    <div className='flex items-center justify-between'>
+                                        <p>کشور</p>
+                                        <span>{data.country}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            "یافت نشد"
+                        )}
+                    </MoviezueqBox>
+                </div>
+            </div>
+        )
+    }
 
     const BackgroundImageUrl = getBgImageSrc(data, isLoading)
 
