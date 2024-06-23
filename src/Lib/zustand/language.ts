452198fa1a71f6bab02b8b4ecdf8d3b088427c11
Erphun <heydar858@gmail.com>
2024-06-23 12:58:03 +0330

    feat: save language into localStorage and use it as deafult value for the next time

--- src/Lib/zustand/language.ts
+++ src/Lib/zustand/language.ts
@@ -1,8 +1,19 @@
 import { create } from "zustand"
 
-const useLanguage = create<languageStore>(set => ({
-    lang: "FA",
-    ChangeLanguage: lang => set(prv => ({ ...prv, lang })),
-}))
+const useLanguage = create<languageStore>(set => {
+    type langT = languageStore["lang"]
+    const localLang = localStorage.getItem("lang")
+    const DefaultLang: langT = localLang === "FA" || localLang === "EN" ? localLang : "FA"
+
+    return {
+        lang: DefaultLang,
+        ChangeLanguage: lang =>
+            set(prv => {
+                localStorage.setItem("lang", lang)
+
+                return { ...prv, lang }
+            }),
+    }
+})
 
 export default useLanguage
